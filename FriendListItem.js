5da743edcbb34a7420acda16a88a7876917ff90e
emily_zhao <emily.zhao@airbnb.com>
2016-07-01 10:31:09 -0700

    PanResponder start

--- FriendListItem.js
+++ FriendListItem.js
@@ -3,26 +3,81 @@ import {
 		StyleSheet,
 		TouchableHighlight,
 		View,
+		PanResponder,
 		Text
 } from 'react-native';
 
 class FriendListItem extends Component {
-	propTypes: { // todo emily not sure if this is doing anything at all
-			friend: React.PropTypes.element.isRequired,
-			onSelect: React.PropTypes.element.isRequired
-			};
+	static propTypes = { // todo emily not sure if this is doing anything at all
+		friend: React.PropTypes.object.isRequired,
+		onSelect: React.PropTypes.func.isRequired
+	};
+
+	constructor(props) {
+		super(props);
+		this._panResponder = {};
+		this.state = {
+			_previousLeft: 0,
+			left: 0,
+			color: '#FFF'
+		};
+	}
 
 	render() {
 		var onSelect = this.props.onSelect;
 		var friend = this.props.friend;
+
+		var dynamicStyle = {
+			left: this.state.left,
+			backgroundColor: this.state.color
+		};
+
 		return (
 				<TouchableHighlight onPress={() => onSelect(friend)}>
-					<View style={styles.friendContainer}>
+					<View
+							style={[styles.friendContainer, dynamicStyle]}
+							{...this._panResponder.panHandlers}>
 						<Text style={styles.name}>{friend.name}</Text>
 					</View>
 				</TouchableHighlight>
 		)
 	}
+
+	componentWillMount() {
+		this._panResponder = PanResponder.create({
+			onStartShouldSetPanResponder: this._handleStartShouldSetPanResponder,
+			onMoveShouldSetPanResponder: this._handleMoveShouldSetPanResponder,
+			onPanResponderGrant: this._handlePanResponderGrant,
+			onPanResponderMove: this._handlePanResponderMove,
+			onPanResponderRelease: this._handlePanResponderEnd,
+			onPanResponderTerminate: this._handlePanResponderEnd
+		});
+	}
+
+	_handleStartShouldSetPanResponder(e: Object, gestureState: Object): boolean {
+		// Should we become active when the user presses down on the circle?
+		return true;
+	}
+
+	_handleMoveShouldSetPanResponder(e: Object, gestureState: Object): boolean {
+		// Should we become active when the user moves a touch over the circle?
+		return true;
+	}
+
+	_handlePanResponderGrant = (e: Object, gestureState: Object) => {
+		this.setState({color: 'blue'});
+	};
+
+	_handlePanResponderMove = (e: Object, gestureState: Object) => {
+		this.setState({left: this.state._previousLeft + gestureState.dx});
+	};
+
+	_handlePanResponderEnd = (e: Object, gestureState: Object) => {
+		this.setState({
+			color: '#FFF',
+			_previousLeft: this.state._previousLeft + gestureState.dx
+		});
+	};
 }
 
 var styles = StyleSheet.create({
@@ -35,7 +90,6 @@ var styles = StyleSheet.create({
 		paddingRight: 12,
 		borderBottomWidth: 1,
 		borderColor: '#EEE',
-		backgroundColor: '#FFF'
 	},
 	name: {
 		fontSize: 14
